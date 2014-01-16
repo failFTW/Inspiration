74466413fd5f4a56e03b81a2fc525138237d1cc7
SilentThief <hexishgames@gmail.com>
2014-01-15 20:00:15 -0800

    Multiple Additions/Changes
    Edited home-page, added top and bottom, added a new photo "noise",
    edited style.css, did some tweaks to config.php, also added a few lines
    of code to the index file.

---
+++ assets/top.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="assets/css/style.css">
+		<link rel="stylesheet" href="assets/css/style.min.css">
+	</head>
+</html>
--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php 
 include_once('core/config.php');
 
-$page = @$_GET['page'];
+$page = @$_GET['page']; //Grabbing pages from the whole root directory. e.g "home"
 switch ($page) {
 	case null:
-	case "home": //Simplistic Home Page
-		$getpage = "#";
+	case "home": //Home Page
+		$getpage = "public/home";
 		break;
 }
+include_once('assets/top.php');
+include_once($getpage.".php");
+include_once('assets/bottom.php');
 ?>
