989f938f9b38c92875b18dd16ddfa9fbaecbdc89
SilentThief <hexishgames@gmail.com>
2014-01-15 20:16:12 -0800

    Multiple Small Additions
    Fixed importing of jquery, added charset, added title.

--- assets/top.php
+++ assets/top.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<title>Inspiration</title>
+		<meta charset="utf-8"/>
 		<link rel="stylesheet" href="assets/css/style.css">
 		<link rel="stylesheet" href="assets/css/style.min.css">
-		<link rel="script" href="core/js/jquery-1.10.2.js">
+		<script src="core/js/jquery-1.10.2.js"></script>
 	</head>
 	<body>
 		<div id="container-navbar">
