4a46978054b83d0c02fe8ac70c0c38a3d2621dec
SilentThief <hexishgames@gmail.com>
2014-01-15 20:13:31 -0800

    Quick Script Addition

--- assets/top.php
+++ assets/top.php
@@ -3,6 +3,7 @@
 	<head>
 		<link rel="stylesheet" href="assets/css/style.css">
 		<link rel="stylesheet" href="assets/css/style.min.css">
+		<link rel="script" href="core/js/jquery-1.10.2.js">
 	</head>
 	<body>
 		<div id="container-navbar">
